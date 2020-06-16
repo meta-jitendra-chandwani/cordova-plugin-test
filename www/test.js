ca2ee44dad6a4f4f00c91fcc27c9746416539f9d
Jitendra Chandwani <jitendra.chandwani@tekizma.com>
2020-06-16 20:37:57 +0530

    changes

--- www/test.js
+++ www/test.js
@@ -4,6 +4,6 @@ var PLUGIN_NAME = "test"; // This is just for code completion uses.
 
 var test = function() {}; 
 test.testFun = function(onSuccess, onError) {
-   exec(onSuccess, onError, PLUGIN_NAME, "yourFunctionName", []);
+   exec(onSuccess, onError, PLUGIN_NAME, "testFun", []);
 };
 module.exports = test;
