bc68c12ad1f6350068564b874052315c26d72cff
Jitendra Chandwani <jitendra.chandwani@tekizma.com>
2020-06-16 18:42:30 +0530

    add

---
+++ www/test.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = "test"; // This is just for code completion uses.
+
+var test = function() {}; 
+test.testFun = function(onSuccess, onError) {
+   exec(onSuccess, onError, PLUGIN_NAME, "yourFunctionName", []);
+};
+module.exports = test;
